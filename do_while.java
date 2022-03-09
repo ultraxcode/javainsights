169cff92d811f693cb0683c13273469dbff4b910
ultraxcode <anuragx19@gmail.com>
2022-03-09 23:41:09 +0530

    Day 7 Do While Loop

---
+++ do_while.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class do_while {
+    public static void main(String[] args) {
+        //do while loop
+        //Do While loop will execute atleast once
+        //Code execute then condition check
+        int b=1000;
+        do{
+            System.out.println(b);
+            b++;
+        }while(b<5);
+    }
+}
