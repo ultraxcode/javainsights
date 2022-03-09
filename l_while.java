b4c8f3ebaffa32e561e840cdf93a72f95cfd74e1
ultraxcode <anuragx19@gmail.com>
2022-03-09 23:03:20 +0530

    Day 7 While Loop

---
+++ l_while.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class l_while {
+    public static void main(String[] args) {
+        int i = 100;
+        //Using While Loop
+        while (i<=200){
+            System.out.println(i);
+            i++;
+        // Note : If the condition never becomes false this will turn into an infinite loop.
+
+
+        }
+    }
+}
