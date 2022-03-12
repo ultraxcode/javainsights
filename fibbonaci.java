fdfdaed00a2743878ad8773f1f9e6e81e506fe9a
ultraxcode <anuragx19@gmail.com>
2022-03-12 14:42:59 +0530

    JAVA Program to Print Fibbonaci Series

---
+++ fibbonaci.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class fibbonaci {
+    public static void main(String[] args) {
+        int n=10;
+        int a=0;
+        int b=1;
+
+        for(int i=0;i<=10;i++){
+          int nextNum=a+b;
+            System.out.println(nextNum);
+            a=b;
+            b=nextNum;
+        }
+    }
+}
