af59a767ef5078ea1efe2553d7a6afa22c86c0e7
ultraxcode <anuragx19@gmail.com>
2022-03-26 01:28:15 +0530

    Recursion in JAVA

---
+++ recursion.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class recursion {
+    static int factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else(){
+            return n*factorial(n-1);
+        }
+    public static void main(String[] args) {
+            int n=4;
+            System.out.println(factorial(n));
+
+
+
+        }
+    }
+}
