686c2eb6432e6fbc122e5a2e8b4c802ec9709968
ultraxcode <anuragx19@gmail.com>
2022-04-02 18:42:39 +0530

    Fibbonaci series using recursion

---
+++ refibbo.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class refibbo {
+    static int fib(int n){
+        if(n==1 || n==2){
+            return n-1;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
+    public static void main(String[] args) {
+        int result = fib(5);
+        System.out.println(result);
+    }
+}
